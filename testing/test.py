3c1c542ef0bd8da6f6c3d9f9e727ba544fa9dc0c
Andres Beltran <89097527+andresbeltranc@users.noreply.github.com>
2024-05-01 09:16:56 -0500

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -4,10 +4,10 @@ str = """ TEST
     string TEST =TEST;
     //std::TEST << (TEST *)TEST << endl;
     return TEST""";
-guacamoleCipher = CryptographyPy()
-encrypted_text = guacamoleCipher.encryptData(str,"password","salt")
+cryptographyPy = CryptographyPy()
+encrypted_text = cryptographyPy.encryptData(str,"password","salt")
 print("encrypted data : ", encrypted_text)
 print("-----------------------------------")
 print("-----------------------------------")
-decrypted_text = guacamoleCipher.decryptData(encrypted_text,"password","salt")
+decrypted_text = cryptographyPy.decryptData(encrypted_text,"password","salt")
 print("decrypted data : ", decrypted_text)
